de3bbc1cf8e853a4ae2e82b876e608c54041b8bd
ravikishore5471 <ravikishore547@gmail.com>
2025-10-14 13:46:11 -0500

    Update Greeter.java

--- Greeter.java
+++ Greeter.java
@@ -1,7 +1,25 @@
 package hello;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class HelloWorld {
+    
     public static void main(String[] args) {
-        System.out.println("Hello, Azure!");
+        SpringApplication.run(HelloWorld.class, args);
+    }
+    
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Azure Web App! Spring Boot is working! 🎉";
+    }
+    
+    @GetMapping("/health")
+    public String health() {
+        return "Application is healthy!";
     }
 }
