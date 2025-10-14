9dbbd96200f5e596906d088df287cdc403552b86
ravikishore5471 <ravikishore547@gmail.com>
2025-10-14 13:42:14 -0500

    Update Greeter.java

--- Greeter.java
+++ Greeter.java
@@ -1,7 +1,7 @@
 package hello;
 
-public class Greeter {
-  public String sayHello() {
-    return "Hello world!!@@@!!";
-  }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Azure!");
+    }
 }
